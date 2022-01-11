a0d1d9a0c26425ce422fa46c0c39911306459a0d
snorlax <68203941+snorlax93@users.noreply.github.com>
2022-01-10 22:22:49 -0500

    initial commit
    initial commit of springboot java backend.

---
+++ src/test/java/com/ragnarokonline/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnarokonline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
