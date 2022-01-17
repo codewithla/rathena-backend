199de94db43a356cbdc3623297f7661047f36c8a
snorlax <68203941+snorlax93@users.noreply.github.com>
2022-01-17 03:25:34 -0500

    index controller
    fixing the type of index controller to responseentity

--- src/main/java/com/ragnarokonline/api/controller/IndexController.java
+++ src/main/java/com/ragnarokonline/api/controller/IndexController.java
@@ -1,8 +1,8 @@
 package com.ragnarokonline.api.controller;
 
 import java.util.HashMap;
-import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public Map<String, String> defaultRoute() {
+    public ResponseEntity<?> defaultRoute() {
         HashMap<String, String> map = new HashMap<>();
         map.put("statusCode", "200");
         map.put("status", "SUCCESS");
         map.put("message", "rathena-online api is online!");
         map.put("data", null);
-        return map;
+        return ResponseEntity.ok(map);
     }
 }
